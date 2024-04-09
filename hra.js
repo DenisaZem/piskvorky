ad6c004a3edd35f0491067ff14a717aa1365bc68
DenisaZem <xdenisa.zemanova@gmail.com>
2024-04-09 07:58:05 +0200

    Přidána prevence nechtěné ukončení hry

--- hra.js
+++ hra.js
@@ -1,11 +1,39 @@
-const HandleClick =(event)=>{
-    let player = "circle"
-    event.target.classList.add(`board__field--${player}`)
-}
+let currentPlayer = "playerCircle";
 
-const SelectSquare = document.querySelectorAll(".square")
-SelectSquare.forEach((item)=>{
-    item.addEventListener("click", HandleClick)
-})
+const HandleClick = (event) => {
+  const imgCurrentPlayer = document.querySelector(".imgCurrentPlayer");
+  if (currentPlayer === "playerCircle") {
+    event.target.classList.add(`board__field--circle`);
+    imgCurrentPlayer.src = "/piskvorky/pictures/cross.svg";
+    currentPlayer = "playerCross";
+  } else {
+    event.target.classList.add(`board__field--cross`);
+    imgCurrentPlayer.src = "/piskvorky/pictures/circle.svg";
+    currentPlayer = "playerCircle";
+  }
+};
 
+const SelectSquare = document.querySelectorAll(".square");
+SelectSquare.forEach((item) => {
+  item.addEventListener("click", HandleClick);
+});
 
+// Prevent game restart
+document
+  .querySelector(".buttons-section__icons--restart")
+  .addEventListener("click", (event) => {
+    const confirmation = confirm("Jste si jisti, že chcete restartovat hru?");
+    if (confirmation === false) {
+      event.preventDefault();
+    }
+  });
+document
+  .querySelector(".buttons-section__icons--home")
+  .addEventListener("click", (event) => {
+    const confirmation = confirm(
+      "Jste si jisti, že chcete ukončit hru a přejít na hlavní stránku?"
+    );
+    if (confirmation === false) {
+      event.preventDefault();
+    }
+  });
